63d4afd1541b7694089d36c99e6f7a17d8d24340
jenjiau <jenlikespirates@hotmail.com>
2017-03-21 18:26:25 -0400

    updates

---
+++ d09/materials/travelling-dwarf/solution.js
@@ -0,0 +1,66 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+// for(i = 0; i < 5; i++){
+//   d.move();
+// }
+
+var i = 0;
+
+while (i < 4) {
+  d.move();
+  i = i + 1;
+}
+
+d.orient("down");
+
+var i = 0;
+
+while (i < 16) {
+  d.move();
+  i = i + 1;
+}
+
+d.orient("right");
+
+var i = 0;
+
+while (i < 6) {
+  d.move();
+  i = i +1
+}
+
+d.orient("up");
+
+var i = 0;
+
+while (i < 11) {
+  d.move();
+  i = i + 1
+}
+
+d.orient("right");
+
+var i = 0
+
+while (i < 6) {
+  d.move();
+  i = i + 1 
+}
+
+d.orient("down");
+
+var i = 0
+
+while (i < 9) {
+  d.move();
+  i = i + 1
+}
+
+d.orient("right");
+
+var i = 0
+
+while (i < 3){
+  d.move(); 
+  i = i + 1
+}
